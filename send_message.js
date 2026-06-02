8ee7d8e8dbff585a3bf3c697356bc5763a3c4641
Villagerzock <marvin.sieber@rsnweb.ch>
2026-06-02 08:55:16 +0200

    Adding send_message.js

---
+++ send_message.js
@@ -0,0 +1,10 @@
+function sendMail(name, subject, message){
+    window.location.href = `mailto:marvin.sieber@edu.gbssg.ch?subject=Message From: ${name} with Subject: ${subject}&body=${message}`
+}
+
+const name = document.getElementById("name");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+document.getElementById("send-message").addEventListener("click",()=>{
+    sendMail(name.value, subject.value, message.value);
+});
